5db9103992c0bbf7bd6ff36c982463da89e33c3b
pmin825 <pmin.0825@gmail.com>
2020-11-02 15:18:33 -0800

    setup entry file

---
+++ bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+console.log('it works');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
