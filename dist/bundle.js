99a906540e338633561a05bb451a0082da015076
pmin825 <pmin.0825@gmail.com>
2020-11-02 22:31:51 -0800

    work on main game function

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,6 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("intro-container").classList.add("hidden");
   });
 });
+window.addEventListener('load', init);
+var time = 10;
+var isPlaying;
+var wordsDisplay = document.querySelector('#words-display');
+var wordsInput = document.querySelector('#words-input');
+var timeDisplay = document.querySelector('#timer');
+var sample = ["He found his art never progressed when he literally used his sweat and tears."];
+
+function init() {
+  showChallenge(sample);
+}
+
+function showChallenge(sample) {
+  wordsDisplay.innerHTML = sample;
+}
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {   
     let startGame = document.getElementById("start-game");
 
     startGame.addEventListener("click", () => { 
         document.getElementById("intro-container").classList.add("hidden");
-
     })
 })
+
+window.addEventListener('load', init);
+
+let time = 10;
+let isPlaying;
+
+const wordsDisplay = document.querySelector('#words-display');
+const wordsInput = document.querySelector('#words-input');
+const timeDisplay = document.querySelector('#timer');
+
+const sample = ["He found his art never progressed when he literally used his sweat and tears."]
+
+function init() {
+    showChallenge(sample)
+}
+
+function showChallenge(sample) {
+    wordsDisplay.innerHTML = sample;
+}
