0c42ec7f9dba982999f6fa34a6698360714fe737
pmin825 <pmin.0825@gmail.com>
2020-11-02 20:32:35 -0800

    create modal and work on beginning game structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let startGame = document.getElementById("start-game");
+
+    startGame.addEventListener("click", () => { 
+        document.getElementById("intro-container").classList.add("hidden");
+
+    })
+})
